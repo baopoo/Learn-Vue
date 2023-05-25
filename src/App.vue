d2a3a8881b315985b7914a69fd0f080db55642d9
Bao Poo <baomien12@gmail.com>
2023-05-25 10:45:06 +0700

    Build: Learn Vue throung simple test

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="row">
+        <app-card :cardWords="card" @resetMyCar="onClickChangeCard()"></app-card>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Card from './components/Card.vue'
+
+  export default {
+    components: {
+      'app-card': Card
+    },
+    data() {
+      return {
+        card: 'BMW'
+      }
+    },
+    methods: {
+      onClickChangeCard(){
+        this.card = 'Suzuki'
+      }
+    },
+    
+
+  }
+</script>
+
+<style>
+  body {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h2>{{ cardWords }}</h2>
+        <p>This is my car !</p>
+        <button @click="resetMyCar()">Change my Car !</button>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        cardWords: {
+            type: String,
+            default: null,
+        },
+    },
+    methods: {
+        resetMyCar(){
+            this.$emit('resetMyCar');
+        }
+    },
+}
+</script>
+<style scoped>
+    div {
+        padding: 30px;
+        margin: 0 auto;
+        border: 1px solid red;
+        text-align: center;
+    }
+    
+</style>
